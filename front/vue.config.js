1fbd4edc3b22cc14914aec397809ddb0cacb6540
Vitaliy K. <vitaliyk@live.com>
2024-11-06 22:06:43 +0300

    after successfull update of vue-cli up to version 5 and several other dependencies and also closing critical flaws

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,10 +1,33 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
-
   pluginOptions: {
-    vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
+    vuetify: {}
+  },
+  // Оптимизации для development
+  configureWebpack: {
+    cache: true,
+    optimization: {
+      removeAvailableModules: false,
+      removeEmptyChunks: false,
+      splitChunks: false,
+    },
+    output: {
+      pathinfo: false
+    },
+    performance: {
+      hints: false
+    }
+  },
+  // Отключаем лишние опции в development
+  css: {
+    sourceMap: false
+  },
+  productionSourceMap: false,
+  lintOnSave: false, // Временно отключаем линтинг при сборке
+  devServer: {
+    hot: true,
+    liveReload: false
   }
 })
