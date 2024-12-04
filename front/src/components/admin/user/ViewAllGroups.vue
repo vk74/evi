829ec3f500d92fe1aa102a48493f7cc2c6e88174
Vitaliy K. <vitaliyk@live.com>
2024-12-04 22:47:42 +0300

    project is ready for TypeScript components now!

--- front/src/components/admin/user/ViewAllGroups.vue
+++ front/src/components/admin/user/ViewAllGroups.vue
@@ -1,2 +1,18 @@
-<template></template>
-<script setup></script>
+<template>
+    <div>
+      <TestCounter />
+      <!-- Тут может быть основной контент ViewAllGroups -->
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue'
+  import TestCounter from '@/components/ts-components/TestCounter.vue'
+  
+  export default defineComponent({
+    name: 'ViewAllGroups',
+    components: {
+      TestCounter
+    }
+  })
+  </script>
