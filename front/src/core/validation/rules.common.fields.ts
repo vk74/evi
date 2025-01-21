ae505f88fce194d279d991702deb2c3683ec325e
Vitaliy K. <vitaliyk@live.com>
2025-01-21 07:33:43 +0300

    updated e-mail regex in validation rules

--- front/src/core/validation/rules.common.fields.ts
+++ front/src/core/validation/rules.common.fields.ts
@@ -39,15 +39,15 @@ export function useValidationRules() {
        emailRules: [
         v => !!v || t('validation.email.required'),
         v => {
-          const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
-          return emailRegex.test(v) || t('validation.email.format')
+          const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
+          return (emailRegex.test(v) && v.length <= 254) || t('validation.email.format')
         }
-       ],
+      ],
 
-       optionalEmailRules: [
+      optionalEmailRules: [
         v => {
-          const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
-          return !v || emailRegex.test(v) || t('validation.email.format')
+          const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
+          return !v || (emailRegex.test(v) && v.length <= 254) || t('validation.email.format')
         }
       ],
 
